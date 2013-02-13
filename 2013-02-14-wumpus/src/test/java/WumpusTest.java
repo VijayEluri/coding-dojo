e092ddf3c598cd30c5616cdd4e0fbb4337dbcb57
Ivar Fredriksen <ivar.fredriksen@iterate.no>
2013-02-13 17:56:39 +0100

    Add dummy test and pom.xml

---
+++ 2013-02-14-wumpus/src/test/java/WumpusTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class WumpusTest {
+
+    @Test
+    public void dummyTest() throws Exception {
+        assertEquals(2, 1 + 1);
+    }
+}
